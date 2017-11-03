6d327f681e5dc11cb69a051d647e96f9b1e8e854
Rangel Ivanov <radito3@mail.bg>
2017-11-03 14:08:13 +0200

    Add Reflection exercise
    Java Reflection exercise

---
+++ Reflection/src/org/elsys/reflection/ConstructorsOf.java
@@ -0,0 +1,18 @@
+package org.elsys.reflection;
+
+import java.lang.reflect.Constructor;
+import java.util.stream.Stream;
+
+public class ConstructorsOf extends AbstractCommand implements Command {
+
+	@Override
+	protected void execute(Class<?> className, String... arguments) {
+		Constructor<?>[] constructors = className.getConstructors();
+
+		System.out.println(Stream.of(constructors)
+				.map(Constructor::toString)
+				.reduce((n1, n2) -> n1 + ", " + n2)
+				.orElse(""));
+	}
+
+}
